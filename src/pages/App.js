e5d1c42d8cb76b9f18d856041c486f80500e0f73
Vitor-Kemuel <kemuvitor@gmail.com>
2022-11-09 23:45:34 -0300

    Criando base do projeto

---
+++ src/pages/App.js
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="App">
+      Olá mundo!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *, body{
+        padding: 0;
+        margin: 0;
+    }
+
+    body {
+        background: #22272E;
+        color: #FFF;
+    }
+`
