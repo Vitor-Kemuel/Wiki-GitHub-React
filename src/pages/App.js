16f67f59381416d195d9351922a5539ecfbb2eec
Vitor-Kemuel <kemuvitor@gmail.com>
2022-11-10 01:20:54 -0300

    Adicionando components e lógica para busca na API

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,31 @@
+import logo from '../assets/github.png';
+import { Container } from './styles';
+import Input from '../components/input';
+import ItemRepo from '../components/itemRepo';
+import { useState } from 'react';
+import Button from '../components/button';
+import { api } from '../services/api';
 
 const App = () => {
+  const [repos, setRepos] = useState([])
+  const [currentSearch, setCurrentSearch] = useState("")
+
+  const handleSearchRepo = async () => {
+    if (currentSearch != "") {
+
+      const { data } = await api.get(currentSearch)
+
+      setRepos(data.items)
+    }
+  }
+
   return (
-    <div className="App">
-      Olá mundo!
-    </div>
+    <Container>
+      <img src={logo} alt="logo" width={72} height={72} />
+      <Input value={currentSearch} onChange={(e) => setCurrentSearch(e.target.value)} />
+      <Button label={"Buscar"} onClick={handleSearchRepo} />
+      {repos.map(repo => <ItemRepo repo={repo} />)}
+    </Container>
   );
 }
 
