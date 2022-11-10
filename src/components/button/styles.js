5e1cd78edcc525e27346bb3d265d01b33d76fc1c
Vitor-Kemuel <kemuvitor@gmail.com>
2022-11-10 01:19:07 -0300

    Adicionando component de botão

---
+++ src/components/button/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    border: 1px solid #fafafa;
+    background: #FAFAFA;
+    border-radius: 20px;
+    height: 62px;
+    min-height: 62px;
+    width: 80%;
+    margin: 20px 0 0;
+
+    &:hover{
+        cursor: pointer;
+        background: #FAFAFA40;
+    }
+`
